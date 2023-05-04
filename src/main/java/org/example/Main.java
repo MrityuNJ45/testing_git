f758f0dab9396b15986fe3c488a45ea62da1e9b0
MrityuNJ45 <mrityunjaysorcar@gmail.com>
2023-05-04 20:57:41 +0530

    second commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,7 @@ public class Main {
         System.out.println("Hello world!");
 
         System.out.println("Hello There mere dost");
+
+        System.out.println("Main hoon hero tera");
     }
 }
